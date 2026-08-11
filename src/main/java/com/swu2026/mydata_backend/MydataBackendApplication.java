b913fdae89bdc63f8a9f1d784d51916f3c1e43c4
TechNing39 <leeyujineee@gmail.com>
2026-08-11 12:21:14 +0900

    chore: Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/swu2026/mydata_backend/MydataBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swu2026.mydata_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydataBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydataBackendApplication.class, args);
+	}
+
+}
